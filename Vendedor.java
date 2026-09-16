725eecd7d2ae5eb8d18fc54b9bcfdf4e31221361
Gui Barbosa <barbosguilhermeh11@gmail.com>
2026-09-16 15:08:03 -0300

    Add files via upload

---
+++ Vendedor.java
@@ -0,0 +1,22 @@
+public class Vendedor extends Funcionario{
+    public float valorVendido;
+    public Vendedor(String nome, String cpf, float salario, float valorVendido) {
+        super(nome, cpf, salario);
+        this.valorVendido = valorVendido;
+    }
+    @Override
+    public float calcularBonificacao() {
+        return 5/100f;
+    }
+    @Override
+    public float calcularComissao() {
+        return this.valorVendido * 5/100;
+    }
+    @Override
+    public float calcularRemuneracaoTotal() {
+        return getSalario() + (getSalario() * calcularBonificacao()) + calcularComissao();
+    }
+    public String toString(){
+        return "Vendedor";
+    }
+}
